feab64832a64fc141f454af16168e7e8034f975e
Arvid Persson <a.persson04@gmail.com>
2024-04-03 00:22:05 +0200

    optimized main imports

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from os import path
 
 from joints import Joint
-from gestures import Gesture
 from visualize import visualize
 
 names = tuple(Joint.headers())
 
 training = pd.read_csv("../data/training.csv", names=names)
 
-visualize(training, 0)
+visualize(training, 100)
