3a2aebea1437946086f71e85ef758104f81b1335
Arvid Persson <a.persson04@gmail.com>
2024-04-03 00:18:38 +0200

    generalized visualize

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from os import path
 
 from joints import Joint
+from gestures import Gesture
+from visualize import visualize
 
 names = tuple(Joint.headers())
 
 training = pd.read_csv("../data/training.csv", names=names)
 
-#print(training.describe())
+visualize(training, 0)
--- src/visualize.py
+++ src/visualize.py
@@ -1,32 +1,43 @@
+import matplotlib
+from matplotlib import pyplot as plt
+
 # override default environment setting; display plot in new window
 # if this raises errors, try `pip install PyQt5`
-import matplotlib
 matplotlib.use("Qt5Agg")
 
-from main import *
+from gestures import Gesture
 from helpers import *
 
-hello_gesture = training.head(1)
-hello_positions = filter_df(hello_gesture, positions=True, means=True)
-xs = filter_df(hello_positions, positions=(True, False, False), means=True)
-ys = filter_df(hello_positions, positions=(False, True, False), means=True)
-zs = filter_df(hello_positions, positions=(False, False, True), means=True)
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(xs, ys, zs)
+def visualize(df: DataFrame, index: int = 0):
+    """
+    Visualizes a gesture as a 3d plot.
+    :param df: the dataframe containing the data.
+    :param index: the index of the gesture to visualize, 0-indexed.
+    """
 
-for i, j in Joint.connections():
-    # adjust offset
-    i = (i - 1) * 3
-    j = (j - 1) * 3
+    hello_gesture = df.iloc[[index]]
 
-    x_i, y_i, z_i = hello_positions.iloc[0, i:i + 3].values
-    x_j, y_j, z_j = hello_positions.iloc[0, j:j + 3].values
-    ax.plot([x_i, x_j], [y_i, y_j], [z_i, z_j])
+    hello_positions = filter_df(hello_gesture, positions=True, means=True)
+    xs = filter_df(hello_positions, positions=(True, False, False), means=True)
+    ys = filter_df(hello_positions, positions=(False, True, False), means=True)
+    zs = filter_df(hello_positions, positions=(False, False, True), means=True)
 
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(xs, ys, zs)
 
-plt.show()
+    for i, j in Joint.connections():
+        # adjust offset
+        i = (i - 1) * 3
+        j = (j - 1) * 3
+
+        x_i, y_i, z_i = hello_positions.iloc[0, i:i + 3].values
+        x_j, y_j, z_j = hello_positions.iloc[0, j:j + 3].values
+        ax.plot([x_i, x_j], [y_i, y_j], [z_i, z_j])
+
+    # ax.set_xlabel('X Label')
+    # ax.set_ylabel('Y Label')
+    # ax.set_zlabel('Z Label')
+
+    plt.show()
