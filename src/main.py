9fd8f2f8c1068348e32d534f57187d6ac132e27e
Arvid Persson <a.persson04@gmail.com>
2024-05-16 21:10:25 +0200

    added value counts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from pandas import value_counts
+
+from data import *
+
+
 def main():
-    pass
+    tr = value_counts(training_targets(), sort=False)
+    tr.sort_index(inplace=True)
+    tr.to_csv("tr.csv", index=False)
+    print(tr)
+    te = value_counts(testing_targets(), sort=False)
+    te.sort_index(inplace=True)
+    te.to_csv("te.csv", index=False)
+    print(te)
 
 
 if __name__ == "__main__":
